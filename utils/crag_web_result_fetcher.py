f65d5792e94109e74ffd57b3bf96d72844d0391c
Tiffany <tma@meta.com>
2026-01-04 19:34:54 -0800

    Update crag_web_result_fetcher.py

--- utils/crag_web_result_fetcher.py
+++ utils/crag_web_result_fetcher.py
@@ -1,3 +1,9 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+
+# This source code is licensed under the license found in the
+# LICENSE file in the root directory of this source tree.
+
 """DO NOT MODIFY THIS FILE.
 
 This file has simple functionality to get the full page content of a web search result.
