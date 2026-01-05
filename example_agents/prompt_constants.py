b0fba16a6826c363578f97d977c47d36287c6099
Tiffany <tma@meta.com>
2026-01-04 19:36:44 -0800

    Update prompt_constants.py

--- example_agents/prompt_constants.py
+++ example_agents/prompt_constants.py
@@ -1,3 +1,9 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+
+# This source code is licensed under the license found in the
+# LICENSE file in the root directory of this source tree.
+
 from pydantic import BaseModel
 
 SYSTEM_PROMPT = """You are a helpful assistant that truthfully answers the user \
