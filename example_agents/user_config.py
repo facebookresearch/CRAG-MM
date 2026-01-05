8b37a713ca2a458fba8b72edba3b18c54d1862f1
Tiffany <tma@meta.com>
2026-01-04 19:37:08 -0800

    Update user_config.py

--- example_agents/user_config.py
+++ example_agents/user_config.py
@@ -1,3 +1,9 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+
+# This source code is licensed under the license found in the
+# LICENSE file in the root directory of this source tree.
+
 from .closed_model_agents import ClaudeAgent, GeminiAgent, GPTAgent
 from .open_source_model_agents import InternVLAgent, LlamaAgent, PixtralAgent, QwenAgent
 
