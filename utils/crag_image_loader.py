f01ece22447ad85b44806dd608ee5baa219925b5
Tiffany <tma@meta.com>
2026-01-04 19:34:41 -0800

    Update crag_image_loader.py

--- utils/crag_image_loader.py
+++ utils/crag_image_loader.py
@@ -1,3 +1,9 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+
+# This source code is licensed under the license found in the
+# LICENSE file in the root directory of this source tree.
+
 """DO NOT MODIFY THIS FILE.
 
 This file contains ImageLoader class which can be used to download and cache images.
